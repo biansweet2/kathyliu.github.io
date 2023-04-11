6ae7d6ca7c9207c859cf217adedf098b488681ea
biansweet2 <kathyliu028@gmail.com>
2023-04-10 21:32:33 -0500

    Update sampleJS.js

--- js/sampleJS.js
+++ js/sampleJS.js
@@ -1,3 +1,14 @@
 function howdy(firstName){
     alert("Howdy " + firstName);
 }
+
+function evalNumber() {
+    let input = document.getElementById("numInput").value;
+    let num = parseInt(input);
+  
+    if (Number.isInteger(num) && num >= 10000 && num <= 99999) {
+        document.getElementById("result").innerHTML = "Valid 5-digit integer";
+    } else {
+        document.getElementById("result").innerHTML = "Invalid input, please enter a 5-digit integer";
+    }
+}
