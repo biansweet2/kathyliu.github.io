d2b2b39c64ab9bc4fdfb110da15b5c44c9678ea4
biansweet2 <kathyliu028@gmail.com>
2023-04-10 20:58:38 -0500

    Update integer.js

--- js/integer.js
+++ js/integer.js
@@ -1,10 +1,10 @@
 function evalNumber() {
-  let input = document.getElementById("numInput").value;
-  let num = parseInt(input);
+    let input = document.getElementById("numInput").value;
+    let num = parseInt(input);
   
-  if (Number.isInteger(num) && num >= 10000 && num <= 99999) {
-    document.getElementById("result").innerHTML = "Valid 5-digit integer";
-  } else {
-    document.getElementById("result").innerHTML = "Invalid input, please enter a 5-digit integer";
-  }
+    if (Number.isInteger(num) && num >= 10000 && num <= 99999) {
+        document.getElementById("result").innerHTML = "Valid 5-digit integer";
+    } else {
+        document.getElementById("result").innerHTML = "Invalid input, please enter a 5-digit integer";
+    }
 }
